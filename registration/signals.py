3dd9715f85e3b30e43bf2ada07d29f1639ce2c19
Styvane Soukossi <4968379+styvane@users.noreply.github.com>
2023-06-27 10:09:50 +0200

    ref signal and remove get_version

--- registration/signals.py
+++ registration/signals.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from django.contrib.auth import get_backends
-from django.contrib.auth import login
+from django.contrib.auth import get_backends, login
 from django.dispatch import Signal
 
 # An admin has approved a user's account
@@ -14,13 +13,13 @@ user_activated = Signal()
 
 
 def login_user(sender, user, request, **kwargs):
-    """ Automatically authenticate the user when activated  """
+    """Automatically authenticate the user when activated"""
     backend = get_backends()[0]  # Hack to bypass `authenticate()`.
-    user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
+    user.backend = f"{backend.__module__}.{backend.__class__.__name__}"
     login(request, user)
-    request.session['REGISTRATION_AUTO_LOGIN'] = True
+    request.session["REGISTRATION_AUTO_LOGIN"] = True
     request.session.modified = True
 
 
-if getattr(settings, 'REGISTRATION_AUTO_LOGIN', False):
+if getattr(settings, "REGISTRATION_AUTO_LOGIN", False):
     user_activated.connect(login_user)
