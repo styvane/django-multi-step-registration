595020096441182df393d525902bd7f2c01bfce7
Styvane Soukossi <4968379+styvane@users.noreply.github.com>
2023-06-27 09:26:31 +0200

    Register registration action with the the @action decorator + minor improvements

--- registration/admin.py
+++ registration/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.sites.shortcuts import get_current_site
+from django.utils.translation import gettext_lazy as _
 
 from .models import RegistrationProfile
-from .users import UsernameField
-from .utils import _
+
+User = get_user_model()
 
 
 class RegistrationAdmin(admin.ModelAdmin):
-    actions = ['activate_users', 'resend_activation_email']
-    list_display = ('user', 'activation_key_expired')
-    raw_id_fields = ['user']
-    search_fields = ('user__{0}'.format(UsernameField()),
-                     'user__first_name', 'user__last_name')
+    actions = ["activate_users", "resend_activation_email"]
+    list_display = ("user", "activation_key_expired")
+    raw_id_fields = ["user"]
+    search_fields = (
+        f"user__{User.USERNAME_FIELD}",
+        "user__first_name",
+        "user__last_name",
+    )
 
+    @admin.action(description=_("Activate users"))
     def activate_users(self, request, queryset):
         """
         Activates the selected users, if they are not already
@@ -23,8 +29,8 @@ class RegistrationAdmin(admin.ModelAdmin):
         site = get_current_site(request)
         for profile in queryset:
             RegistrationProfile.objects.activate_user(profile.activation_key, site)
-    activate_users.short_description = _("Activate users")
 
+    @admin.action(description=_("Re-send activation emails"))
     def resend_activation_email(self, request, queryset):
         """
         Re-sends activation emails for the selected users.
@@ -39,9 +45,9 @@ class RegistrationAdmin(admin.ModelAdmin):
         site = get_current_site(request)
         for profile in queryset:
             user = profile.user
-            RegistrationProfile.objects.resend_activation_mail(user.email, site, request)
-
-    resend_activation_email.short_description = _("Re-send activation emails")
+            RegistrationProfile.objects.resend_activation_mail(
+                user.email, site, request
+            )
 
 
 admin.site.register(RegistrationProfile, RegistrationAdmin)
