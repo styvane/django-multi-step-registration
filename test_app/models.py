3fafa6bc32127fc9d7db3b5d9a2590c551933039
Styvane Soukossi <4968379+styvane@users.noreply.github.com>
2023-06-27 14:16:42 +0200

    reformat and update test app

--- test_app/models.py
+++ test_app/models.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 
@@ -7,4 +6,4 @@ class CustomUser(AbstractBaseUser):
     new_field = models.CharField(max_length=25)
     objects = BaseUserManager()
 
-    USERNAME_FIELD = 'new_field'
+    USERNAME_FIELD = "new_field"
